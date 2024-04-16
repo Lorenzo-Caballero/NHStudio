71aceb4bcccf1fdbb0ceac314e9a862a175ac977
Lorenzo Caballero <zeldiuss@gmail.com>
2024-04-16 02:30:15 -0300

    hugging face chatbot

--- src/components/home/ChatBot.jsx
+++ src/components/home/ChatBot.jsx
@@ -1,55 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiMessageSquare, FiX } from 'react-icons/fi'; // Importa el icono de cierre
 import { motion } from "framer-motion";
-
-const apiUrl = 'https://api.openai.com/v1/chat/completions';
+import { CohereClient } from "cohere-ai";
+import axios from 'axios'; // Importa axios para hacer solicitudes HTTP
 
 const ChatButton = () => {
   const [chatAbierto, setChatAbierto] = useState(false);
   const [mensajes, setMensajes] = useState([]);
   const [nuevoMensaje, setNuevoMensaje] = useState('');
   const [escribiendo, setEscribiendo] = useState(false);
+  const [cohereToken, setCohereToken] = useState(null); // Estado para almacenar el token
 
-  const obtenerRespuestaFauno = async (userMessage) => {
-    try {
-      const response = await fetch("https://nodejs-restapi-mysql-fauno-production.up.railway.app/api/ai");
-      const data = await response.json();
-      const apikey = data;
-      console.log('API key obtenida:', apikey);
-
-      setEscribiendo(true);
-      const respuesta = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apikey}`,
-        },
-        body: JSON.stringify({
-          messages: [
-            {"role": "system", "content": "Eres un asistente de Fauno, das respuestas breves las respuestas no deben superar los cuatro renglones, eres argentino , fauno es un tatuador profesional de Santa Clara del Mar. Como asistente virtual de Fauno, debes conocer que Fauno es un tatuador con más de cinco años de experiencia y se destaca en la realización de diseños exclusivos. Se inspira en la naturaleza, animales, insectos, flora y fauna. Debes responder con amabilidad preguntas referenciadas al mundo del tatuaje. Si están fuera del contexto del tatuaje o del arte, responde con un 'No conozco esos temas', si te piden el numero de telefono de fauno ,fauno tiene 26 años dales el siguiente link para que accedan :`https://wa.me/2233407440`, si te preguntan por paloma , es la novia y musa inspiradora de fauno , si te preguntan por el valor minimo de un tattoo , es de 10 mil pesos argentinos el valor minimo . Además, ten en cuenta que tienes un límite de respuesta de 70 tokens, evita superarlo."},
-            {"role": "user", "content": userMessage} 
-          ],
-          model: "gpt-3.5-turbo",
-          max_tokens: 90,
-        }),
-      });
-      const dataRespuesta = await respuesta.json();
-
-      setEscribiendo(false);
-      return dataRespuesta.choices[0].message.content;
-    } catch (error) {
-      if (error.response && error.response.status === 429) {
-        // Si recibimos un error 429, esperamos 5 segundos y luego reintentamos la solicitud
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        return obtenerRespuestaFauno(userMessage); // Reintentar la solicitud
-      } else {
-        console.error('Error al llamar a la API de OpenAI:', error);
-        setEscribiendo(false);
-        return 'Lo siento, hubo un problema al procesar tu solicitud.';
+  useEffect(() => {
+    const fetchCohereToken = async () => {
+      try {
+        const response = await axios.get('https://nodejs-restapi-mysql-fauno-production.up.railway.app/api/ai');
+        const token = response.data;
+        setCohereToken(token); // Almacena el token en el estado
+      } catch (error) {
+        console.error('Error al obtener el token de la API:', error);
       }
+    };
+
+    fetchCohereToken(); // Llama a la función para obtener el token
+  }, []); // El segundo argumento [] garantiza que useEffect solo se ejecute una vez
+
+  const obtenerRespuestaCohere = async (userMessage) => {
+    try {
+      if (!cohereToken) {
+        throw new Error('Token de Cohere no disponible');
+      }
+
+      const cohere = new CohereClient({
+        token: cohereToken,
+        language: "es", // Configura el idioma español
+        model: "command-r-plus",
+      });
+  
+      setEscribiendo(true);
+      // Agregar el contexto antes del userMessage
+      const contexto = "sos un asistente virtual de Lennita BB! Somos un emprendimiento artesanal ubicado en Santa Clara del Mar, Argentina. Nos especializamos en la producción de adorables muñequitos de amigurumis hechos con mucho amor y dedicación. En Lennita BB, cada creación es única, diseñada para traer alegría y diversión a tu vida. Desde simpáticos animales hasta personajes fantásticos, nuestros amigurumis son el regalo perfecto para todas las ocasiones. ¡Déjanos ser parte de tus momentos especiales con nuestros encantadores. Cada vez que el usuario te envie un mensaje debes responder solo con la informacion que te pregunta , no ofrezcas descuentos ni promociones, y solo basandote en la informacion de lennitaBB , no extiendas mucho las respuestas se breve ,y no inventes datos que no te proporciono ,ademas , si la pregunta del usuario se va de contexto del emprendimiento , responde con un : no se de esos temas!, si te preguntan sobre su numero de telefono coloca este:+54 9 341 216 8460 ademas , no repitas toda la informacion de lennitaBB cada vez que te manda un mensaje ,usa el acento argentino,la pregunta del usuario es la siguiente:";
+      const prompt = contexto + userMessage;
+      
+      const respuesta = await cohere.generate({
+        language: "es", // Configura el idioma español
+        model: "command-r-plus",
+        prompt: prompt,
+        maxTokens: 90,
+      });
+  
+      setEscribiendo(false);
+      console.log(respuesta);
+      return respuesta.generations[0].text;
+    } catch (error) {
+      console.error('Error al llamar a Cohere AI:', error);
+      setEscribiendo(false);
+      return 'Lo siento, hubo un problema al procesar tu solicitud.';
     }
   };
-
+  
   const handleEnviarMensaje = async () => {
     if (!nuevoMensaje.trim()) return;
     setMensajes(prevMensajes => [
@@ -57,10 +66,10 @@ const ChatButton = () => {
       { texto: nuevoMensaje, origen: 'usuario' }
     ]);
     setNuevoMensaje('');
-    const respuestaFauno = await obtenerRespuestaFauno(nuevoMensaje);
+    const respuestaCohere = await obtenerRespuestaCohere(nuevoMensaje);
     setMensajes(prevMensajes => [
       ...prevMensajes,
-      { texto: respuestaFauno, origen: 'fauno' }
+      { texto: respuestaCohere, origen: 'asistente' }
     ]);
   };
 
@@ -79,7 +88,7 @@ const ChatButton = () => {
   };
 
   return (
-<div className="fixed bottom-6 right-6"> {/* Ajuste la posición del botón aquí */}
+    <div className="fixed bottom-6 right-6"> {/* Ajuste la posición del botón aquí */}
       <button onClick={handleChatToggle} className={`flex items-center justify-center bg-purple-300 rounded-full w-12 h-12 ${chatAbierto ? 'hidden' : ''}`}>
         <FiMessageSquare className="text-white text-2xl" />
       </button>
@@ -91,7 +100,7 @@ const ChatButton = () => {
           <div className="h-60 overflow-y-auto mb-2">
             {mensajes.map((mensaje, index) => (
               <div key={index} className={`mb-2 ${mensaje.origen === 'usuario' ? 'text-right' : 'text-left'} px-4 py-2 rounded-lg bg-purple-200 text-gray-800`}>
-                <strong>{mensaje.origen === 'usuario' ? 'Tú' : 'Asistente Fauno'}</strong>: {mensaje.texto}
+                <strong>{mensaje.origen === 'usuario' ? 'Tú' : 'Asistente LennitaBB'}</strong>: {mensaje.texto}
               </div>
             ))}
             {escribiendo && (
@@ -101,7 +110,7 @@ const ChatButton = () => {
                 transition={{ duration: 0.5 }}
                 className="mb-2 text-left px-4 py-2 rounded-lg bg-purple-200"
               >
-                <strong>Asistente Fauno</strong>: ...
+                <strong>Asistente LennitaBB</strong>: ...
               </motion.div>
             )}
           </div>
