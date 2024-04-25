7e5ffd0926bd4110459361f2f2ef8c4ae904d422
Lorenzo Caballero <zeldiuss@gmail.com>
2024-04-24 22:24:47 -0300

    animation line in products on scroll

--- src/components/home/Overview.jsx
+++ src/components/home/Overview.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
@@ -22,11 +22,11 @@ const Overview = () => {
             animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : 20 }}
             exit={{ opacity: 0, y: 20 }}
             transition={{ duration: 0.5 }}
-            className="w-full bg-purple-200 py-20 md:py-32 text-center"
+            className="w-full bg-[#F1D8F4] py-20 md:py-32 text-center text-[#9F5D23]"
         >
             <div className="px-3 lg:px-0 lg:w-[55%] mx-auto">
-                <h2 className="font-extrabold text-5xl text-[#242833] capitalize mb-10 tracking-widest leading-10">Lennita BB</h2>
-                <p className="text-lg text-[#555] tracking-widest font-normal mb-5 leading-7">¡Bienvenidos a Lennita BB! Somos un emprendimiento artesanal ubicado en Santa Clara del Mar, Argentina. Nos especializamos en la producción de adorables muñequitos de amigurumis hechos con mucho amor y dedicación. En Lennita BB, cada creación es única, diseñada para traer alegría y diversión a tu vida. Desde simpáticos animales hasta personajes fantásticos, nuestros amigurumis son el regalo perfecto para todas las ocasiones. ¡Déjanos ser parte de tus momentos especiales con nuestros encantadores muñequitos de amigurumis!</p>
+                <h2 className="font-extrabold text-5xl capitalize mb-10 tracking-widest leading-10">Lennita BB</h2>
+                <p className="text-lg tracking-widest font-normal mb-5 leading-7">¡Bienvenidos a Lennita BB! Somos un emprendimiento artesanal ubicado en Santa Clara del Mar, Argentina. Nos especializamos en la producción de adorables muñequitos de amigurumis hechos con mucho amor y dedicación. En Lennita BB, cada creación es única, diseñada para traer alegría y diversión a tu vida. Desde simpáticos animales hasta personajes fantásticos, nuestros amigurumis son el regalo perfecto para todas las ocasiones. ¡Déjanos ser parte de tus momentos especiales con nuestros encantadores muñequitos de amigurumis!</p>
                 <ScrollLink className="inline-block px-6 py-3 font-semibold tracking-wider text-white bg-purple-400 uppercase mt-8 text-lg hover:bg-purple-300" to="about" smooth={true} duration={500}>
                     Más sobre nosotros
                 </ScrollLink>
