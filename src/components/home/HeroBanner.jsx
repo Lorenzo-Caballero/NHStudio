46d4b56f4ac8433aba779a1a2c1096f49080b433
Lorenzo Caballero <zeldiuss@gmail.com>
2025-02-28 04:49:17 -0300

    portada fix

--- src/components/home/HeroBanner.jsx
+++ src/components/home/HeroBanner.jsx
@@ -3,12 +3,9 @@ import videoFile from "./video.mp4"; // Ajusta la ruta según corresponda
 
 const HeroBanner = () => {
   return (
-    <div className="relative w-full h-screen flex flex-col md:flex-row items-center justify-center">
-      {/* Barra de navegación */}
-
-
+    <div className="w-full h-screen flex flex-col md:flex-row items-center justify-center md:mt-0 md:pt-0 mt-5 pt-20">
       {/* Contenedor del video */}
-      <div className="w-full md:w-1/2 h-full relative mt-20 md:mt-0 z-10">
+      <div className="w-full md:w-1/2 h-full relative z-10">
         <video
           className="w-full h-full object-cover"
           autoPlay
@@ -22,7 +19,7 @@ const HeroBanner = () => {
       </div>
 
       {/* Contenedor del texto */}
-      <div className="w-full md:w-1/2 h-full flex items-center justify-center p-8 bg-gradient-to-br from-[#4779a4] to-[#f7e7ce] text-white-900 mt-20 md:mt-0">
+      <div className="w-full md:w-1/2 h-full flex items-center justify-center p-8 bg-gradient-to-br from-[#4779a4] to-[#f7e7ce] text-white-900 md:mt-0 mt-0">
         <div className="max-w-lg text-center md:text-left">
           <h1 className="text-3xl md:text-4xl font-bold mb-6 text-white">TATTOO</h1>
           <p className="text-base md:text-lg leading-relaxed text-[#2c3e50]">
